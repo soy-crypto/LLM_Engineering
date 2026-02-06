da01c06584d8272b61cb85457e19666500c9d41e
peter zhu <ztjnwu@gmail.com>
2026-02-06 00:32:48 +0000

    02/05

--- bm.py
+++ bm.py
@@ -69,7 +69,7 @@ def get_model(model_name: str, device: str) -> Tuple[PreTrainedModel, torch.dtyp
     else:
         torch_dtype = torch.float32
 
-    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch_dtype).to(device)
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch_dtype,low_cpu_mem_usage=True).to(device)
     model.eval()
 
     #return
