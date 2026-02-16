0ef02d80c5e5dc977567ea0ccae83ee9e9eb09ac
peterzhu <ztjnwu@gmail.com>
2026-02-16 05:45:14 +0000

    fix

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-/workspace/LLM_Engineering/benchmarks/hf/run_hf.sh
+#/workspace/LLM_Engineering/benchmarks/hf/run_hf.sh
 /workspace/LLM_Engineering/benchmarks/vllm/run_vllm.sh
 /workspace/LLM_Engineering/benchmarks/trt/run_trt.sh
--- benchmarks/vllm/run_vllm.sh
+++ benchmarks/vllm/run_vllm.sh
@@ -3,9 +3,9 @@ set -e
 
 source /workspace/LLM_Engineering/.venv_vllm/bin/activate
 
-python /workspace/LLM_Engineering/vllm/bm_vllm.py \
+python /workspace/LLM_Engineering/benchmarks/vllm/bm_vllm.py \
   --model Qwen/Qwen2.5-7B-Instruct \
-  --prompts /workspace/prompts/prompts_mid.txt \
+  --prompts /workspace/LLM_Engineering/prompts/prompts_mid.txt \
   --batch_size 1,2,4,8,16 \
   --max_new_tokens 512 \
   --runs 3 \
