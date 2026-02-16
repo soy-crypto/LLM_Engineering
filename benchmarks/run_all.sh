1132fb324095b6d6c9d2f7082746ca718fa3c0bc
peterzhu <ztjnwu@gmail.com>
2026-02-16 05:56:27 +0000

    fix

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -2,5 +2,5 @@
 set -e
 
 #/workspace/LLM_Engineering/benchmarks/hf/run_hf.sh
-/workspace/LLM_Engineering/benchmarks/vllm/run_vllm.sh
+#/workspace/LLM_Engineering/benchmarks/vllm/run_vllm.sh
 /workspace/LLM_Engineering/benchmarks/trt/run_trt.sh
