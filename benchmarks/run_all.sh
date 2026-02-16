0e844dd4bfc519a02e3e1e78529bfd226b052972
peterzhu <ztj_nwu@snnu.edu.cn>
2026-02-15 19:12:22 -0800

    update

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-/workspace/LLM_Engineeringbenchmarks/hf/run_hf.sh
-/workspace/LLM_Engineeringbenchmarks/vllm/run_vllm.sh
-/workspace/LLM_Engineeringbenchmarks/trt/run_trt.sh
+/workspace/LLM_Engineering/benchmarks/hf/run_hf.sh
+/workspace/LLM_Engineering/benchmarks/vllm/run_vllm.sh
+/workspace/LLM_Engineering/benchmarks/trt/run_trt.sh
