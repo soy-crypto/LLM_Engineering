1da1e3a583fe49cf377315542d7d49f8117b7be4
peter zhu <ztjnwu@gmail.com>
2026-02-17 20:08:18 -0800

    Update bootstrap_llama.sh

--- benchmarks/bootstrap_llama.sh
+++ benchmarks/bootstrap_llama.sh
@@ -8,7 +8,7 @@ HF_MODEL_DIR="$PROJECT/hf_models/llama3_1_8b"
 CKPT_DIR="$PROJECT/trt_ckpt/llama3_1_8b_bf16_1gpu"
 ENGINE_DIR="$PROJECT/trt_engine/llama3_1_8b_bf16_b16_s2560"
 
-
+PYTHON_BIN="python3"
 #######################################
 # HF venv
 #######################################
