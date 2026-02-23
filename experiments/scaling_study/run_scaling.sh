941a0ac089cf3f4786eabf68becca81ebfd44abc
Peterzhu <ztjnwu@gmail.com>
2026-02-23 02:16:43 +0000

    0220

--- experiments/scaling_study/run_scaling.sh
+++ experiments/scaling_study/run_scaling.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-PY="/workspace/.venv_vllm/bin/python"
+VENV="/workspace/.venv_vllm"
 SCRIPT="/workspace/LLM_Engineering/experiments/scaling_study/run_vllm_scaling.py"
 MODEL="/workspace/hf_models/llama3_1_8b"
 
-$PY "$SCRIPT" \
+source "$VENV/bin/activate"
+
+python "$SCRIPT" \
     --model "$MODEL" \
     --batches 1,2,4,8 \
     --context 512 \
     --decode 128
+
+deactivate
